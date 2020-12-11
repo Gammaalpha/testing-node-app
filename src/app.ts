6999ebe331a7548ba5d871368252886d7eff9978
Faiq Malik <faiqmalik2@gmail.com>
2020-12-11 00:25:15 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,28 @@ function test() {
     // const regex = /\b(html|test)\b/gi;
     // const regex = new RegExp(`\\b(${pattern})\\b`, "i")
     // const regex2 = new RegExp(/[*]\[([^\]]*)\]:\s*(.+)\n*/)
-    const string = "test ";
+    const string = "**test**";
+    const string2 = "**test \nderp **";
 
-    console.log(utilRightSpace(string));
 
-
+    // const regex = new RegExp(/[(\*|\_|\~){0,}]/, "i");
+    const regex2 = new RegExp(/([*_~]*)([a-zA-Z0-9 ]+)([*_~]*)/, "gi");
+    // const regex3 = new RegExp(/(\*{1,2}|\_{1,2}|\~{1,2})/, "i")
+    console.log(utilGetSideChars(string2, regex2));
 };
 
-
-function utilRightSpace(val: string): boolean {
-    let valLen = val.length;
-    return val[valLen-1] === " " ? true : false
-};
+/**
+ * Utility to test and get special characters on the sides of the inputted string. If they are the same, the side character(s) are returned, else null is returned.
+ * @param val String input to test
+ * @param regex Regex to test against
+ */
+function utilGetSideChars(val: string, regex: RegExp): string | null {
+    const result = (val.split(regex).filter(x => x !== ""));
+    if (result[0] === result[result.length - 1]) {
+        return result[0]
+    }
+    return null;
+}
 
 
 test();
