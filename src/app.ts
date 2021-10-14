52568cd1098a31442b8844ed8e3be5ce45825e5d
Faiq Malik <faiqmalik2@gmail.com>
2021-10-14 15:47:10 -0400

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
-import sumIndexTarget from "./subsetSum";
-
 function testFunc() {
 
-    const result = sumIndexTarget([3, 2, 7, 1], 6)
 
 }
 
---
+++ test/demo.test.ts
@@ -0,0 +1,12 @@
+// DEMO testing
+// describe('SumIndexTarget Test Null Check', () => {
+
+//     it("Is empty 1", () => {
+//         const result = subsetSum([], 2);
+//         expect(result).toStrictEqual([])
+//     })
+//     it("Is empty 2", () => {
+//         const result = subsetSum([0, 3], 0);
+//         expect(result).toStrictEqual([])
+//     })
+// })
