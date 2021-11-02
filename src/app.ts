3ff7a9f5baad03ef92c009bc36bcee18df4a5c5d
Faiq Malik <faiqmalik2@gmail.com>
2021-11-01 22:37:36 -0400

    added rxjs and updated ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
-function testFunc() {
-
-
+import { from, of } from "rxjs";
+import { concatMap, delay } from "rxjs/operators"
+function observableWithDelay() {
+    let arr: number[] = [1, 2, 3]
+    from(arr).pipe(
+        concatMap(
+            (item: number) => of(item).pipe(delay(1000))
+        )
+    ).subscribe(
+        (timedItem: number) => {
+            console.log(timedItem);
+        }
+    )
 }
 
-testFunc()
+observableWithDelay()
 
 
